f174c0a2b7f23345143483bd8cfa14a7c1e8e55d
yvargasp1 <yavargasp1@ucentral.edu.co>
2021-10-11 21:49:33 -0500

    Home-image product

--- lib/src/models/products.dart
+++ lib/src/models/products.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class Product {
-  final String? image, title, description;
+  final String image, title, description;
   final int? price, size, id;
   final Color? color;
 
   Product(
-      {this.image,
-      this.title,
-      this.description,
+      {required this.image,
+     required this.description,
+       required this.title,
       this.price,
       this.size,
       this.id,
@@ -16,7 +16,7 @@ class Product {
 }
 
 List<Product> products = [
-  Product(id: 1,title: 'Lava 1',price: 10,size: 12,description: 'aa',image: 'assets/screenpage.png',color: Colors.black),
+  Product(id: 1,title: 'Lava 1',price: 10,size: 12,description: 'aa',image: 'assets/washing1.png',color: Colors.white),
   Product(id: 2,title: 'Lava 2',price: 10,size: 12,description: 'aa',image: 'assets/screenpage.png',color: Colors.green),
 
   
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -7,72 +7,82 @@ import 'package:productos_app/src/widgets/widgets.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+   
+    return Stack(
+      children: [
+        BackgroundImage(),
+        Scaffold(
+      backgroundColor: Colors.transparent,
       appBar: buildAppBar(context),
-      body: 
-      SafeArea(child: 
-      Column(
+      body: SafeArea(
+          child: Column(
         children: [
           Container(
-           height: 100,
-           child: Center(
-           child:  
-          Text('Modelos',
-          style: TextStyle(fontSize: 20,
-          color: Colors.black,
-          decorationStyle: TextDecorationStyle.wavy
+            height: 100,
+            child: Center(
+              child: Text(
+                'Modelos',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                    decorationStyle: TextDecorationStyle.wavy),
+              ),
+            ),
           ),
+          SizedBox(
+            height: 30,
           ),
-           ), 
-          )
-          
+          Container(
+              height: 180,
+              width: 160,
+              decoration: BoxDecoration(
+                  color: products[0].color,
+                  borderRadius: BorderRadius.circular(16)),
+              child: Image(image: AssetImage(products[0].image)))
         ],
-      )
-      )
-      ,
+      )),
+    ),
+      ],
     );
+
+    
   }
 
-  AppBar buildAppBar(context){
+  AppBar buildAppBar(context) {
     return AppBar(
-        backgroundColor: Colors.blue[100],
-        leading: GestureDetector(
-          onTap: (){
-           Navigator.push( context,
-           MaterialPageRoute(
-           builder: (context) => RegisterPage()));
-
-          },
-          child: Icon(
-            Icons.arrow_back,
-            color: Colors.black,
-          ),
+      backgroundColor: Colors.blue[100],
+      leading: GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => RegisterPage()));
+        },
+        child: Icon(
+          Icons.arrow_back,
+          color: Colors.black,
         ),
-        actions:<Widget> [
-          Padding(padding:EdgeInsets.only(right: 20.0) ,
+      ),
+      actions: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(right: 20.0),
           child: GestureDetector(
-            onTap:(){},
+            onTap: () {},
             child: Icon(
               Icons.search,
               size: 26.0,
             ),
           ),
-          ),
-          Padding(padding:EdgeInsets.only(right: 20.0) ,
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: 20.0),
           child: GestureDetector(
-            onTap:(){},
+            onTap: () {},
             child: Icon(
               Icons.history,
               size: 26.0,
             ),
           ),
-          )  
-
-
-        ],
-        
-      );
-    
+        )
+      ],
+    );
   }
-        
 }
